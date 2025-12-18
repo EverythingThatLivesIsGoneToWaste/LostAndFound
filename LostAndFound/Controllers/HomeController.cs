8761e854a7e285d91db12a196e5e06a2ca2b0ef2
EverythingThatLivesIsGoneToWaste <kurianovip@gmail.com>
2025-12-18 21:52:17 +0400

    update home controller

--- LostAndFound/Controllers/HomeController.cs
+++ LostAndFound/Controllers/HomeController.cs
@@ -1,28 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using LostAndFound.Services;
 
 namespace LostAndFound.Controllers
 {
-    public class HomeController(IDateTime dateTime) : Controller
+    public class HomeController() : Controller
     {
-        private readonly IDateTime _dateTime = dateTime;
-
         public IActionResult Index()
         {
-            var serverTime = _dateTime.Now;
-            if (serverTime.Hour < 12)
-            {
-                ViewData["Message"] = "It's morning here - Good Morning!";
-            }
-            else if (serverTime.Hour < 17)
-            {
-                ViewData["Message"] = "It's afternoon here - Good Afternoon!";
-            }
+            if (User.Identity.IsAuthenticated)
+                return View(); //TODO Authorized user
             else
-            {
-                ViewData["Message"] = "It's evening here - Good Evening!";
-            }
-            return View();
+                return View();
         }
 
         public IActionResult Privacy()
